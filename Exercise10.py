df538fd052dfff03b5864b17634e1df89fa18969
ChristianoDaniel <65127463+ChristianoDaniel@users.noreply.github.com>
2020-10-03 16:39:00 +0100

    Update Exercise10.py

--- Exercise10.py
+++ Exercise10.py
@@ -5,6 +5,7 @@
 # and write a program that returns a list that contains only the elements that are common between....
 # ...the lists (without duplicates).Make sure your program works on two lists of different sizes.
 # Randomly generate two lists to test this
+
 import random
 
 a = random.sample(range(1, 51), 30)
